c367f07471c51816806c2c00d2e8f22adfd563e4
hananbs158@gmail.com <hananbs158@gmail.com>
2020-12-30 16:53:32 +0200

    Added new Handlers (list/create/update and place a bid)

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,30 +1,39 @@
-import { v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
+import createError from 'http-errors';
+import { v4 as uuid } from 'uuid';
+import commonMiddleware from '../lib/commonMiddleware';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
   const now = new Date();
 
   const auction = {
-    id : uuid(),
+    id: uuid(),
     title,
     status: 'OPEN',
-    createdAt : now.toISOString()
+    createdAt: now.toISOString(),
+    highestBid: {
+      amount: 0,
+    }
   }
 
-  await dynamodb.put({
-    TableName: 'AuctionTable',
-    Item: auction
-  }).promise();
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction
+    }).promise();
 
-  return {
-    statusCode: 201,
-    body: JSON.stringify(auction),
-  };
+    return {
+      statusCode: 201,
+      body: JSON.stringify(auction),
+    };
+  } catch (error) {
+
+    console.error(error)
+    throw new createError.InternalServerError(error);
+  }
 }
 
-export const handler = createAuction;
-
-
+export const handler = commonMiddleware(createAuction);
